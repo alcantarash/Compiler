1bde586d343817ab1fe359d09c488a6b4f212376
Sheldon <sheldon@localhost.localdomain>
2017-09-18 11:02:58 -0300

    Continue Class Lexer

---
+++ src/compiler/Lexer/Lexer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiler.Lexer;
+
+import java.util.Hashtable;
+
+/**
+ *
+ * @author sheldon
+ */
+public class Lexer {
+    
+    public static int line = 1;
+    private char c = ' ';
+    private Hashtable words = new Hashtable();
+    private FileReader file;
+}
