bca9b91fd47939ff7aaa2b6ca37497df8c53a13b
Sheldon <sheldon@localhost.localdomain>
2017-09-18 14:42:17 -0300

    Continue Class Lexer

--- src/compiler/Lexer/Lexer.java
+++ src/compiler/Lexer/Lexer.java
@@ -58,7 +58,86 @@ public class Lexer {
         return true;
     }
 
-    /*public Token scan(){
-    
-    }*/
+    public Token scan() throws IOException {
+
+        for (;; readch()) {
+            if (c == ' ' || c == '\t' || c == '\r' || c == '\b') {
+                continue;
+            } else if (c == '\n') {
+                line++;
+            } else {
+                break;
+            }
+        }
+        //Conferir se está correto
+        switch (c) {
+            case '=':
+                if (readch('=')) {
+                    return Word.eq;
+                } else {
+                    return Word.assign;
+                }
+
+            case '>':
+                if (readch('=')) {
+                    return Word.le;
+                } else {
+                    return new Token('>');
+                }
+            case '<':
+                if (readch('=')) {
+                    return Word.ge;
+                } else {
+                    return new Token('<');
+                }
+            case '!':
+                if (readch('=')) {
+                    return Word.ne;
+                } else {
+                    return Word.not;
+                }
+            case '|':
+                if (readch('|')) {
+                    return Word.or;
+                } else {
+                    return new Token('|');
+                }
+            case '&':
+                if (readch('&')) {
+                    return Word.and;
+                } else {
+                    return new Token('&');
+                }
+        }
+
+        if (Character.isDigit(c)) {
+            int value = 0;
+            do {
+                value = 10 * value + Character.digit(c, 10);
+                readch();
+            } while (Character.isDigit(c));
+            return new Num(value);
+        }
+
+        //Identificadores
+        if (Character.isLetter(c)) {
+            StringBuffer sb = new StringBuffer();
+            do {
+                sb.append(c);
+                readch();
+            } while (Character.isLetterOrDigit(c));
+            String s = sb.toString();
+            Word w = (Word) words.get(s);
+            if (w != null) {
+                return w; //palavra já existe na HashTable
+            }
+            w = new Word(s, Tag.ID);
+            words.put(s, w);
+            return w;
+        }
+
+        Token t = new Token(c);
+        c = ' ';
+        return t;
+    }
 }
