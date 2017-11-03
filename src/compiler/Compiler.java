fc418f1311c9b3662f39a0fe2fa968bd056d3f5e
Sheldon <sheldon@localhost.localdomain>
2017-11-03 11:48:23 -0200

    Updated

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -8,12 +8,12 @@
 package compiler;
 
 import Lexer.Lexer;
+import Syntactic.Parser;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 public class Compiler {
-    
+
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
         String EOF = "@";//Fim de Arquivo
@@ -26,30 +26,44 @@ public class Compiler {
         String endt5 = "teste5.txt";
         String endt6 = "teste6.txt";
         String end = "";
-        do{
+        do {
             //Menu de Opções
             System.out.println("***Compilador - Libério/Larissa/Sheldon***");
             System.out.println("[1] Teste 1");
             System.out.println("[2] Teste 2");
             System.out.println("[3] Teste 3");
             System.out.println("[4] Teste 4");
-            System.out.println("[5] Teste 5");        
+            System.out.println("[5] Teste 5");
             System.out.println("[6] Teste 6");
             System.out.println("Digite o npumero do teste que deseja compilar:");
-            opc = in.nextInt();        
+            opc = in.nextInt();
 
-            switch(opc){
-                case 1: end = endt1;break;
-                case 2: end = endt2;break;
-                case 3: end = endt3;break;
-                case 4: end = endt4;break;
-                case 5: end = endt5;break;
-                case 6: end = endt6;break;
-                default: System.out.println("Opção Invalida");break;
+            switch (opc) {
+                case 1:
+                    end = endt1;
+                    break;
+                case 2:
+                    end = endt2;
+                    break;
+                case 3:
+                    end = endt3;
+                    break;
+                case 4:
+                    end = endt4;
+                    break;
+                case 5:
+                    end = endt5;
+                    break;
+                case 6:
+                    end = endt6;
+                    break;
+                default:
+                    System.out.println("Opção Invalida");
+                    break;
             }
-        }while(opc < 1 || opc >6);
-        
-        Lexer lexer = new Lexer(end);
+        } while (opc < 1 || opc > 6);
+
+        /*Lexer lexer = new Lexer(end);
         String s = lexer.scan().toString();
         System.out.println("\nSequência de Tokens:\n");
         
@@ -61,7 +75,11 @@ public class Compiler {
         System.out.println("\nTabela de símbolos:\n");
         lexer.ImprimeSymbolTable();
 
-        System.out.println("\nCompilado com Sucesso, Sem erros Léxicos.");
+        System.out.println("\nCompilado com Sucesso, Sem erros Léxicos.");*/
+        Lexer lex = new Lexer(end);
+        Parser syn = new Parser(lex);
+        syn.program();
+
     }
-    
+
 }
