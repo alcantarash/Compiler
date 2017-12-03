ec5ee00133d5aab749ebadcac864023dd429f866
liberioafonso <liberioafonso@gmail.com>
2017-12-03 16:53:56 -0200

    Add files via upload

---
+++ src/Semantic/ReadStmt.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semantic;
+
+import java.io.*;
+import Lexer.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author liber
+ */
+public class ReadStmt extends Parser{
+    
+    Identifier identifier;
+
+	public ReadStmt(Parser head) {
+		super(head);
+	}
+
+	@Override
+	public void analise () {
+        switch (token.tag) {
+
+            case Tag.SCAN: 
+
+                {                    
+                    try {
+                        eat(Tag.SCAN);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ReadStmt.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+                {
+                    try {
+                        eat('(');
+                    } catch (IOException ex) {
+                        Logger.getLogger(ReadStmt.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+                identifier = new Identifier(this);
+                identifier.analise();
+                
+                {
+                    try {
+                        eat(')');
+                    } catch (IOException ex) {
+                        Logger.getLogger(ReadStmt.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+                break;
+                
+                default:
+                    System.out.println("Erro sintático na linha " + Lexer.line + ":\n" + "Comando de entrada (READ) não foi encontrado.");
+                    erro();
+        }
+	}
+}
